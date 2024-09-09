2c0129cf03bc8c25c1a0a61e74651a165ae36d47
GiridharanS1729 <giridharans1729@gmail.com>
2024-09-09 19:02:29 +0530

    updated header,footer 090920241902

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,52 +1,54 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import './st.css';
+import React from 'react';
+import './Footer.css';
 import { FaGithub, FaTwitter, FaBehance, FaLinkedin, FaMedium, FaTelegram } from "react-icons/fa";
-export default function Footer() {
-    return (
-        <div>
-            <footer class="footer">
-                <div class="container">
-                    <div class="footer-content">
-                        <div class="footer-about">
-                            <h3>About Us</h3>
-                            <p>Sharing thoughts and ideas on various topics. Join us on our journey of exploration and discovery.</p>
-                        </div>
-                        <div class="footer-links">
-                            <h3>Quick Links</h3>
-                            <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">Contact</a></li>
-                            </ul>
-                        </div>
-                        <div class="footer-social">
-                            <h3>Follow Us</h3>
-                            <ul>
-                                <li><a href="#"><FaLinkedin /></a></li>
-                                <li><a href="#"><FaTwitter /></a></li>
-                                <li><a href="#"><FaTelegram /></a></li>
-                                <li><a href="#"><FaGithub /></a></li>
-                                <li><a href="#"><FaMedium /></a></li>
-                                <li><a href="#"><FaBehance /></a></li>
 
-                            </ul>
-                        </div>
-                        <div class="footer-subscribe">
-                            <h3>Subscribe</h3>
-                            <form>
-                                <input type="email" placeholder="Your email address" />
-                                <button type="submit">Subscribe</button>
-                            </form>
-                        </div>
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer-content">
+                    <div className="footer-about">
+                        <h3>About Us</h3>
+                        <p>Sharing thoughts and ideas on various topics. Join us on our journey of exploration and discovery.</p>
                     </div>
-                    <div class="footer-bottom">
-                        <p>&copy; 2024 Giridharan blogs . All rights reserved.</p>
+
+                    <div className="footer-links">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="footer-social">
+                        <h3>Follow Us</h3>
+                        <ul className="social-icons">
+                            <li><a href="#"><FaLinkedin /></a></li>
+                            <li><a href="#"><FaTwitter /></a></li>
+                            <li><a href="#"><FaTelegram /></a></li>
+                            <li><a href="#"><FaGithub /></a></li>
+                            <li><a href="#"><FaMedium /></a></li>
+                            <li><a href="#"><FaBehance /></a></li>
+                        </ul>
+                    </div>
+
+                    <div className="footer-subscribe">
+                        <h3>Subscribe</h3>
+                        <form>
+                            <input type="email" placeholder="Your email address" />
+                            <button type="submit">Subscribe</button>
+                        </form>
                     </div>
                 </div>
-            </footer>
 
-        </div>
-    )
+                <div className="footer-bottom">
+                    <p>&copy; 2024 Giridharan blogs. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
+    );
 }
+
+export default Footer;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import './ts.css';
-export default function Header() {
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
     return (
-        <div>
-            <header className="App-header">
-                <p className='logo'>Blogging</p>
-                <div className='nav-itm'>
-                    <a className='nav-lnk' href='/'>Home</a>
-                    <a className='nav-lnk' href='/createpost'>CreatePost</a>
-                    <a className='nav-lnk' href='/about'>About</a>
-                    <a className='nav-lnk' href='/contact'>Contact</a>
-                    <a className='nav-lnk' href='/allusers'>All Users</a>
-                </div>
-            </header>
-        </div>
-    )
+        <header className="header">
+            <div className="header-logo">CreativeBrand</div>
+            <nav className="header-nav">
+                <ul className="nav-list">
+                    <li><a className="nav-lnk" href="/">Home</a></li>
+                    <li><a className="nav-lnk" href="/createpost">Create Post</a></li>
+                    <li><a className="nav-lnk" href="/about">About</a></li>
+                    <li><a className="nav-lnk" href="/contact">Contact</a></li>
+                    <li><a className="nav-lnk" href="/allusers">All Users</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
+
+export default Header;
+
