df1ebccff5c6f34bb1c07b88c084f70ebfd0edac
GiridharanS1729 <giridharans1729@gmail.com>
2024-09-09 23:54:12 +0530

    "Updated Footer and Header Css 090920242354

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -15,10 +15,10 @@ const Footer = () => {
                     <div className="footer-links">
                         <h3>Quick Links</h3>
                         <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/settings">Settings</a></li>
+                            <li><a href="/contact">Contact</a></li>
                         </ul>
                     </div>
 
